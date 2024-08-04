26234be36e17ff760dab78a31a26444bd9e92bdf
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-08-04 13:48:02 -0600

    Did Spicy challenge FizzBuzz

---
+++ code_challenges/code_challenges.rb
@@ -0,0 +1,19 @@
+## Spicy
+
+# Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer.
+#  If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". 
+# If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
+
+# input = 3
+
+# if (input % 3 ==0 ) && (input % 5 !=0)
+#     puts "Fizz"
+
+# elsif (input % 3 != 0) && (input % 5 ==0)
+#     puts "Buzz"
+
+# elsif (input % 3 == 0) && (input % 5 == 0)
+#     print "FizzBuzz"
+# else
+#     print input
+# end
